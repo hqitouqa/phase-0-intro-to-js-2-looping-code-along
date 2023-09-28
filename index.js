74b22597fcc89cee2b7c67f0f7745393862b48fd
Hala Qitouqa <h.qitouqa@gmail.com>
2023-09-28 11:00:20 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+names=["Guadalupe", "Ollie", "Aki"]
+message='surprise'
+function writeCards(names,message){
+    let messages=[];
+    for(let i=0; i<3; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+    }
+    return messages;
+}
+
+function  countDown(x){
+    
+    while(x>=0){
+        console.log(x);
+        x--;
+    }
+}
